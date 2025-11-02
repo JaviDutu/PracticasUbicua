9c50714d4c9c8050eaf41b89e8635ef35e8b0654
Javier Dutu <javidutu@gmail.com>
2025-11-02 20:37:54 +0100

    Json perfeccionado.

--- PL1/semaforo/MQTT.hpp
+++ PL1/semaforo/MQTT.hpp
@@ -16,16 +16,22 @@ String obtenerTimestamp() {
   return String(timeString);
 }
 
+// Variable global para almacenar el timestamp de la última publicación exitosa.
+// Inicializada a Epoch Time (1970-01-01T00:00:00Z).
+String lastMqttPublishTimestamp = "1970-01-01T00:00:00Z";
+
 void publicarDatosMQTT() {
   StaticJsonDocument<512> doc;
 
-  // 1. Datos de identidad
+  // --- Datos de identidad --- 
   doc["sensor_id"] = "TL_001_G9";
   doc["sensor_type"] = "traffic_light";
   doc["street_id"] = "ST_1370"; // Vuestro Street ID
   doc["timestamp"] = obtenerTimestamp();
 
-  // 2. Crear y rellenar el objeto "location"
+
+
+  // --- Objeto "location" ---
   JsonObject location = doc.createNestedObject("location");
   location["latitude"] = 40.4087123;
   location["longitude"] = -3.6924532;
@@ -34,22 +40,35 @@ void publicarDatosMQTT() {
   location["neighborhood"] = "Palacio";
   location["postal_code"] = "28005";
 
-  // 3. Objeto "data"
+
+
+  // --- Objeto "data" ---
   JsonObject data = doc.createNestedObject("data");
-  
+  unsigned long tiempoTranscurrido = millis() - tiempoAnteriorEstado;
+  unsigned long tiempoRestante_ms = 0;
+
   String estadoStr = "unknown";
-  if (estadoActual == ESTADO_VERDE) estadoStr = "green";
-  else if (estadoActual == ESTADO_AMARILLO) estadoStr = "yellow";
-  else if (estadoActual == ESTADO_ROJO) estadoStr = "red";
-  
-  data["current_state"] = estadoStr;
-  data["pedestrian_request"] = peticionPeaton;
+  if (estadoActual == ESTADO_VERDE) {estadoStr = "green"; if (peticionPeaton) tiempoRestante_ms = TIEMPO_VERDE_MINIMO - tiempoTranscurrido; else tiempoRestante_ms = TIEMPO_VERDE_MAXIMO - tiempoTranscurrido;}
+  else if (estadoActual == ESTADO_AMARILLO) {estadoStr = "yellow"; tiempoRestante_ms = TIEMPO_AMARILLO - tiempoTranscurrido;}
+  else if (estadoActual == ESTADO_ROJO) {estadoStr = "red"; tiempoRestante_ms = TIEMPO_ROJO - tiempoTranscurrido;}
+  else if (estadoActual == ESTADO_EMERGENCIA) estadoStr = "emergencia";
   
   int lecturaLDR = analogRead(PIN_LDR);
-  float porcentajeLuz = map(lecturaLDR, 200, 950, 0, 100);
-  data["light_level_percent"] = constrain(porcentajeLuz, 0, 100);
+  String brillo = ""; if (lecturaLDR > LDR_TRESHOLD) brillo = "Brillo_al_Maximo"; else {brillo = "Brillo_Bajo";}
 
-  // 4. Serializar y publicar
+  data["last_message_published"] = lastMqttPublishTimestamp;
+  data["traffic_light_type"] = "mixed_vehicle_pedestrian";
+  data["circulation_direction"] = "bidirectional";
+  data["current_state"] = estadoStr;
+  data["pedestrian_request"] = peticionPeaton;
+  data["light_level_percent"] = brillo;
+  data["current_state_seconds"] = tiempoTranscurrido / 1000;
+  if (estadoActual != ESTADO_EMERGENCIA) data["current_state_seconds_left"] = tiempoRestante_ms / 1000;
+  data["uptime_seconds"] = millis() / 1000;
+
+
+
+  // --- Serializar y publicar ---
   char buffer[512];
   size_t n = serializeJson(doc, buffer);
 
@@ -59,5 +78,6 @@ void publicarDatosMQTT() {
   bool exito = mqttClient.publish(mqtt_topic, buffer, n);
   if (exito) {
     Serial.println("ÉXITO");
+    lastMqttPublishTimestamp = currentTimestamp;
   }
 }
